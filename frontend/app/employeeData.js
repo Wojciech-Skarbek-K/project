1a787952b6352751e7981f0a546e794a916fc61d
Maciej Kaczor <maciej.kaczor@kainos.com>
2022-06-23 13:45:31 +0200

    Get data from backend to frontend

--- frontend/app/employeeData.js
+++ frontend/app/employeeData.js
@@ -5,7 +5,7 @@ const axios = require('axios')
 exports.getEmployees = async () => { 
     let employees = []  
       try {  
-        const employeeResponse = await axios.get('http://localhost:8080/api/')
+        const employeeResponse = await axios.get('http://localhost:8080/api/employees')
         employees = employeeResponse.data
       } catch (e) {
          return new Error('Could not get employees')
--- frontend/app/routes.js
+++ frontend/app/routes.js
@@ -3,18 +3,13 @@ const router = express.Router()
 const testData = require('./testData.js')
 const employeeData = require('./employeeData.js')
 
-// Add your routes here - above the module.exports line
-// router.get('/employees', async (req, res) => {
+router.get('/employees', async (req, res) => {
 
-//   res.render('employeesView', {
-//     employees: await employeeData.getEmployees()
-//   }
-//   ); 
+  res.render('employeesView', {
+    employees: await employeeData.getEmployees()
+  }
+  ); 
 
-// });
-
-router.get('/employees', function(req, res){ 
-  res.render('employeesView', { employees: testData.getEmployees() } ); 
 });
 
 module.exports = router
